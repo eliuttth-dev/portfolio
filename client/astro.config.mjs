14c3ebdbc9883848b931c2e3fc17d630e77b6fcb
mizumint <eliuttth@gmail.com>
2025-03-19 15:40:46 +0100

    Add: Server tuneling | Fix: Safari styling bugs

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  server: {
+    host: '0.0.0.0', // Allows access from other devices
+    port: 4321,
+    allowedHosts: [
+      '3730-91-116-116-82.ngrok-free.app',
+    ],
   },
 });
